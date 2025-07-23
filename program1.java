ec1f0e20616eeea6b4f7b0b6ab479f7dc04cd9ae
hari-033 <2420030244@klh.edu.in>
2025-07-23 11:24:34 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class program1 {
+	public static void main(String args[]) {
+		int x= 2;
+		while(x<=5) {
+			System.out.println("hello");
+			x++;
+		}
+	}
+}
---
+++ program2.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class program2 {
+	public static void main(String args[]) {
+		int x= 2, i;
+		for(i= 0; i< 5; i++) {
+			System.out.println("hello");
+		}
+	}
+}
