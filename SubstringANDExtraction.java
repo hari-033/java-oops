52d9f2fb6ed911ab8cc7e6e97b3df25e02c214c1
hari-033 <2420030244@klh.edu.in>
2025-08-06 10:56:53 +0530

    Add files via upload

---
+++ SubstringANDExtraction.java
@@ -0,0 +1,9 @@
+package oops;
+
+public class SubstringANDExtraction {
+	public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.substring(5));         
+        System.out.println(s.substring(2, 7));       
+    }
+}
